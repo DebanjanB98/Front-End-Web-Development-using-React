33d0b162786f4aee7ad0437d1d0fe6e8efa825c7
DebanjanB98 <dbardhan004@gmail.com>
2020-10-31 21:09:29 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
     import { Control, LocalForm, Errors } from 'react-redux-form';  
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { Loading } from './LoadingComponent';
 
 class CommentForm extends Component{
     constructor(props) {
@@ -164,8 +165,25 @@ function RenderComments({comments, addComment, dishId}){
 }
 const DishDetail=(props)=>{
     const item=props.dish;
-    
-    if(item!=null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)
     return(
     <div className="container">
         <div className="row">
@@ -190,9 +208,8 @@ const DishDetail=(props)=>{
       />
         </div>
     </div>
-    </div>);}
-else{
-return( <div></div>);}
+    </div>);
+
 }
 
 export default DishDetail;
